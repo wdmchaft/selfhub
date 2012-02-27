0bda914a75873e9f19b609ef85bc6465683593ce
EvgenKor <evgen2k7@yandex.ru>
2012-02-28 01:18:10 +0400

    Garbage Files were deleted

--- build/SelfHub.build/Debug-iphoneos/SelfHub.build/Objects-normal/armv7/MainInformation.d
+++ build/SelfHub.build/Debug-iphoneos/SelfHub.build/Objects-normal/armv7/MainInformation.d
@@ -1,4 +1,4 @@
 dependencies: \
   /Projects/SelfHub/SelfHub/Modules/MainInformation/MainInformation.m \
   /Projects/SelfHub/SelfHub/Modules/MainInformation/MainInformation.h \
-  /Projects/SelfHub/SelfHub/Modules/ModuleGlobalDefinitions.h
+  /Projects/SelfHub/SelfHub/Modules/ModuleHelper.h
